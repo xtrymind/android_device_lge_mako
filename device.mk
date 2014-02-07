f87efad740459488bfe9aaf5c1e9bc732d6b1d3c
Dede Dindin Qudsy <xtrymind@gmail.com>
2014-02-07 13:36:58 +0700

    remove lw

--- device.mk
+++ device.mk
@@ -37,9 +37,7 @@ PRODUCT_PACKAGES += \
 
 # Live Wallpapers
 PRODUCT_PACKAGES += \
-        LiveWallpapers \
         LiveWallpapersPicker \
-        VisualizationWallpapers \
         librs_jni
 
 PRODUCT_COPY_FILES += \
